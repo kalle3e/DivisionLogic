04805e261cab42081100f512262e67d32b511aa5
kalle3e <64237549+kalle3e@users.noreply.github.com>
2021-03-21 07:58:19 +1100

    Add files via upload

--- divisionLogic.php
+++ divisionLogic.php
@@ -1,5 +1,4 @@
 <?php
-$print = false;
     for ($i=1; $i <=100; $i++) {
         if ($i % 3 == 0) {
             $test[$i]['divisibleBy3'] = true;
@@ -8,18 +7,16 @@ $print = false;
         if ($i % 5 == 0) {
             $test[$i]['divisibleBy5'] = true;
             $test[$i]['FBFB'] = true;
-
-            if ($i == 100) {
-                $print = true;
-            }
         }
+
         appendMessage($i,$test,$message);
-    }
-    if ($print) {
-        echo $message;
+        $test = [];
+        if ($i == 100) {
+            echo $message;
+        }
     }
 function appendMessage($i,$test, &$message){
-//    Output 1, 2, foo, 4, bar, foo, 7, 8, foo, bar, 11, foo, 13, 14, foobar ...
+//    Expect sample output 1, 2, foo, 4, bar, foo, 7, 8, foo, bar, 11, foo, 13, 14, foobar ...foo,97, 98, foo, bar
 
     if ($test[$i]['divisibleBy3'] && !$test[$i]['divisibleBy5']){
         $message .= 'foo,' . ' ';
